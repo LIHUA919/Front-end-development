fd5dace788a5bd9bbaf5c195ad7b12038b5ae618
LIHUA919 <1017343802@qq.com>
2024-11-30 10:50:18 +0800

    upload

--- Minecraft/Assets/Scripts/Blocks/Block.cs
+++ Minecraft/Assets/Scripts/Blocks/Block.cs
@@ -22,3 +22,49 @@ public class Block
         IsBreakable = type != BlockType.Air;
     }
 }
+
+
+
+// Block.cs
+public enum BlockType 
+{
+    Air,
+    Grass,
+    Dirt,
+    Stone,
+    Wood,
+    Leaves,
+    Sand,
+    Water
+}
+
+public class Block 
+{
+    public BlockType Type { get; private set; }
+    public Vector3 Position { get; private set; }
+    public bool IsTransparent { get; private set; }
+    public float Hardness { get; private set; }
+
+    public Block(BlockType type, Vector3 position)
+    {
+        Type = type;
+        Position = position;
+        InitBlockProperties(type);
+    }
+
+    private void InitBlockProperties(BlockType type)
+    {
+        switch(type)
+        {
+            case BlockType.Grass:
+                IsTransparent = false;
+                Hardness = 0.6f;
+                break;
+            case BlockType.Wood:
+                IsTransparent = false;
+                Hardness = 2.0f;
+                break;
+            // 其他方块类型
+        }
+    }
+}
